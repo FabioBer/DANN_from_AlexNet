292fd1ed8eecfed407ce104e20a31c0aa0e3fe3f
FabioBer <58628606+FabioBer@users.noreply.github.com>
2019-12-22 17:53:41 +0100

    Update DANN_from_AlexNet.py

--- DANN_from_AlexNet.py
+++ DANN_from_AlexNet.py
@@ -71,7 +71,7 @@ class AlexNet(nn.Module):
         self.domain_classifier.apply(init_weights)
         
     def forward(self, input_data, alpha):
-        input_data = input_data.expand(input_data.data.shape[0], 3, 28, 28)
+        input_data = input_data.expand(input_data.data.shape[0], 3, 224, 224)
         feature = self.features(input_data)
         feature = feature.view(-1, 256 * 6 * 6)
         reverse_feature = ReverseLayerF.apply(feature, alpha)
