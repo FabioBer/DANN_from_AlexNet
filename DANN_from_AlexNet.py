6470fc98cabb4362a635bbe5c0288d59973e25ef
FabioBer <developerFABBER94>
2019-12-22 09:31:56 +0100

    first commit

---
+++ DANN_from_AlexNet.py
@@ -0,0 +1,70 @@
+import torch.nn as nn
+from torch.utils import load_state_dict_from_url
+from functions import ReverseLayerF
+
+
+__all__ = ['AlexNet', 'alexnet']
+
+
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
+
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=7):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        
+        state_dict = load_state_dict_from_url(model_urls['alexnet'],
+                                              progress=progress,
+                                              strict = False)
+        
+        self.class_classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+        self.class_classifier.load_state_dict(state_dict)
+        
+        
+        self.class_classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 2),
+        )
+        self.class_classifier.load_state_dict(state_dict)
+
+
+    def forward(self, input_data, alpha):
+        feature = self.feature(input_data)
+        feature = feature.view(-1, 256 * 6 * 6)
+        reverse_feature = ReverseLayerF.apply(feature, alpha)
+        class_output = self.class_classifier(feature)
+        domain_output = self.domain_classifier(reverse_feature)
+
+        return class_output, domain_output
