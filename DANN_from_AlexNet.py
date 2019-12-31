175647335d69396aabe135ff5c2fdb73b037ab4c
FabioBer <58628606+FabioBer@users.noreply.github.com>
2019-12-31 09:27:51 +0100

    Update DANN_from_AlexNet.py

--- DANN_from_AlexNet.py
+++ DANN_from_AlexNet.py
@@ -50,8 +50,8 @@ class AlexNet(nn.Module):
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            #nn.Linear(4096, 4096),
-            #nn.ReLU(inplace=True),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
             nn.Linear(4096, num_domains)
         )
         
